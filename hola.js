3649b38205bbd6bcc7a1785f992906122c4a9678
Conrado Javier Teves <javiteves@gmail.com>
2020-03-24 11:42:04 -0300

    Segundo Commit

--- hola.js
+++ hola.js
@@ -1,2 +1,13 @@
 alert("Hola Mundo");
 console.log("Hola Mundo");
+//Creo una clase Objeto
+var objeto = {
+    nombre : "Objeto",
+    entidad: "Entidad",
+    operaciones: "Operaciones",
+
+    metodo : function() {
+        return true;
+    }
+
+}
